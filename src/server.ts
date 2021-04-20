fee51cea2d8f5151404848493e846fdb786e9c9b
Leonardo Estevao <leooestevao@gmail.com>
2021-04-20 15:20:14 -0300

    started project with: Express, typescript, ts-node, and basics configs

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+app.listen(8080, () => {
+    console.log('Server running');
+});
+
+app.get('/', (req, res) => {
+    res.send('Rota funcionando');
+})
+
+app.post('/users', (req, res) => {
+    res.json({message: 'Rota User funcionando'});
+})
