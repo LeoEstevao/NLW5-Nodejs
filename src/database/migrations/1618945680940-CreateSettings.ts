8f5941a588554c5c4196984309cd8db7bd348ef0
Leonardo Estevao <leooestevao@gmail.com>
2021-04-20 17:40:56 -0300

    Added typeOrm. Config database connection. Created "settings" migration

---
+++ src/database/migrations/1618945680940-CreateSettings.ts
@@ -0,0 +1,46 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateSettings1618945680940 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'settings',
+                columns: 
+                [
+                    {
+                        name: 'id',
+                        type: 'varchar',
+                        isPrimary: true,
+                        generationStrategy: 'uuid'
+                    },
+                    {
+                        name: 'username',
+                        type: 'varchar'
+                    },
+                    {
+                        name: 'chat',
+                        type: 'boolean',
+                        default: true
+                    },
+                    {
+                        name: 'update_at',
+                        type: 'timestamp',
+                        default: 'now()'
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()'
+                    }
+                    
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('settings');
+    }
+
+}
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import './database/index';
 
 const app = express();
 
